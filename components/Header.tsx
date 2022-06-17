ed77f9a23898df4b90f24d92fa8adb2f4f2babd8
Eunbi Kang <jenabill@naver.com>
2022-06-17 23:29:32 +0900

    Design: 스타일 컴포넌트 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,30 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-import { theme } from "../styles/theme";
+import styled from "styled-components";
 
 interface HeaderProps {
   header: string;
   changeHeader: (header: string) => void;
 }
 
+const Menu = styled(Text)<{ header: string; menu: string }>`
+  font-size: 38px;
+  font-weight: 600;
+  color: ${({ header, menu, theme }) =>
+    header === menu ? theme.textColor : theme.disable};
+`;
+
 export default function Header({ header, changeHeader }: HeaderProps) {
   return (
     <View style={styles.header}>
       <TouchableOpacity onPress={() => changeHeader("work")}>
-        <Text
-          style={{
-            ...styles.btnText,
-            color: header === "work" ? "white" : theme.grey,
-          }}
-        >
+        <Menu header={header} menu="work">
           Work
-        </Text>
+        </Menu>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => changeHeader("travel")}>
-        <Text
-          style={{
-            ...styles.btnText,
-            color: header === "travel" ? "white" : theme.grey,
-          }}
-        >
+        <Menu header={header} menu="travel">
           Travel
-        </Text>
+        </Menu>
       </TouchableOpacity>
     </View>
   );
@@ -38,10 +35,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     marginTop: 100,
-  },
-  btnText: {
-    color: "white",
-    fontSize: 38,
-    fontWeight: "600",
   },
 });
