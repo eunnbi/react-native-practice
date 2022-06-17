9c8af69d0ce2b7aef61cd3b0750d0120c53111f0
Eunbi Kang <jenabill@naver.com>
2022-06-17 23:29:02 +0900

    Refactor: Context API 이용 및 스타일 컴포넌트 추가

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
-import { useDispatch } from "react-redux";
-import { addTodoItem } from "../modules/todos";
-import { addTravelItem } from "../modules/travels";
+import { useContext, useState } from "react";
+import { TextInput, View } from "react-native";
+import { todosDispatchContext } from "../context/TodosProvider";
+import { travelsDispatchContext } from "../context/TravlesProvider";
+import styled from "styled-components";
+
+const StyledInput = styled(TextInput)`
+  background-color: #fff;
+  padding: 15px 20px;
+  margin: 30px 0;
+  border-radius: 30px;
+  font-size: 18px;
+  border: ${({ theme }) =>
+    theme.name === "light" && `1px solid ${theme.disable}`};
+`;
 
 export default function Input({ header }: { header: string }) {
-  const dispatch = useDispatch();
+  const { addItem: addTodo } = useContext(todosDispatchContext);
+  const { addItem: addTravel } = useContext(travelsDispatchContext);
   const [input, setInput] = useState("");
   const onChangeText = (input: string) => {
     setInput(input);
@@ -15,37 +26,26 @@ export default function Input({ header }: { header: string }) {
       return;
     }
     if (header === "work") {
-      dispatch(addTodoItem(input));
+      addTodo(input);
     } else {
-      dispatch(addTravelItem(input));
+      addTravel(input);
     }
     setInput("");
   };
   return (
     <View>
-      <TextInput
+      <StyledInput
         onSubmitEditing={onSubmit}
         onChangeText={onChangeText}
         autoCapitalize="sentences"
         autoCorrect
         returnKeyType="done"
-        style={styles.input}
         value={input}
         placeholder={
           header === "work" ? "Add a To Do" : "Where do you want to go?"
         }
+        placeholderTextColor={"#B8BAC0"}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: "#fff",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    marginVertical: 30,
-    borderRadius: 30,
-    fontSize: 18,
-  },
-});
