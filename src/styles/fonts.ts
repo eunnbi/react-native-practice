26b449cd808b6478e9af3364fdac609de7b4ed98
Eunbi Kang <jenabill@naver.com>
2022-07-06 03:09:01 +0900

    Add: font styles

---
+++ src/styles/fonts.ts
@@ -0,0 +1,10 @@
+const FONTS = {
+  REGULAR: "Regular",
+  BOLD: "Bold",
+  LIGHT: "Light",
+  EXTRA_BOLD: "ExtraBold",
+  EXTRA_LIGHT: "ExtraLight",
+  SEMI_BOLD: "SemiBold",
+};
+
+export default FONTS;
