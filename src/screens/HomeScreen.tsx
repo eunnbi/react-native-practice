6f0b7dd05c0c022e85e85b90cf6435365806edf0
Eunbi Kang <jenabill@naver.com>
2022-07-05 23:14:42 +0900

    Refactor: add Header to HomeScreen component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,46 +2,30 @@ import { Theme, useTheme } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { useMemo } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import COLORS from "../styles/color";
+import { StyleSheet, ScrollView } from "react-native";
+import GenreList from "../components/Genres/GenreList";
+import Header from "../components/Header";
 import { RootStackParamList } from "../types";
 
 type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
 
-const HomeScreen = (props: HomeScreenProps) => {
+function HomeScreen(props: HomeScreenProps) {
   const theme = useTheme();
   const styles = useMemo(() => makeStyles(theme), []);
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <StatusBar translucent backgroundColor={theme.colors.background} />
-      <View style={styles.headerContainers}>
-        <Text style={styles.headerTitle}>Now Playing</Text>
-        <Text style={styles.headerSubTitle}>VIEW ALL</Text>
-      </View>
+      <Header title="Now Playing" />
+      <GenreList />
     </ScrollView>
   );
-};
+}
 
 const makeStyles = (theme: Theme) =>
   StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: theme.colors.background,
-    },
-    headerContainers: {
-      marginTop: 70,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-    },
-    headerTitle: {
-      fontSize: 28,
-    },
-    headerSubTitle: {
-      fontSize: 13,
-      color: COLORS.active,
     },
   });
 
