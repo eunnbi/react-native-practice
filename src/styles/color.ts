c2a0054ba32bcb0edd9876f2a14b0cbba19fe47b
Eunbi Kang <jenabill@naver.com>
2022-07-05 22:13:06 +0900

    Add: color and theme for styling

---
+++ src/styles/color.ts
@@ -0,0 +1,10 @@
+const COLORS = {
+  active: "#057dfe",
+  heart: "#f13939",
+  yellow: "#ffc107",
+  gray: "#7c7c7c",
+  lightGray: "#969696",
+  secondLightGray: "#e5e5e5",
+};
+
+export default COLORS;
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "@react-navigation/native";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "#f5f7fa",
+  },
+};
+
+export default theme;
