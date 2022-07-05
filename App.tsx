608a35c38d3a6d9c4dfe51e302db7d2ffd528d63
Eunbi Kang <jenabill@naver.com>
2022-07-06 03:12:08 +0900

    Refactor: add useFonts and QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -5,24 +5,44 @@ import HomeScreen from "./src/screens/HomeScreen";
 import MovieScreen from "./src/screens/MovieScreen";
 import { RootStackParamList } from "./src/types";
 import theme from "./src/styles/theme";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const RootStack = createStackNavigator<RootStackParamList>();
+const queryClient = new QueryClient();
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Regular: require("./assets/fonts/NunitoSans-Regular.ttf"),
+    Bold: require("./assets/fonts/NunitoSans-Bold.ttf"),
+    Black: require("./assets/fonts/NunitoSans-Black.ttf"),
+    ExtraBold: require("./assets/fonts/NunitoSans-ExtraBold.ttf"),
+    ExtraLight: require("./assets/fonts/NunitoSans-ExtraLight.ttf"),
+    Light: require("./assets/fonts/NunitoSans-Light.ttf"),
+    SemiBold: require("./assets/fonts/NunitoSans-SemiBold.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <NavigationContainer theme={theme}>
-      <RootStack.Navigator initialRouteName="Home">
-        <RootStack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <RootStack.Screen
-          name="Movie"
-          component={MovieScreen}
-          options={{ headerShown: false }}
-        />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer theme={theme}>
+        <RootStack.Navigator initialRouteName="Home">
+          <RootStack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Movie"
+            component={MovieScreen}
+            options={{ headerShown: false }}
+          />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
