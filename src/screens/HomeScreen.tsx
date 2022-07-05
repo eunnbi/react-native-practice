e7bc3c89d09e906d24566807441c24377a331770
Eunbi Kang <jenabill@naver.com>
2022-07-06 03:11:44 +0900

    Refactor: HomeScreen component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,8 +3,9 @@ import { StackScreenProps } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { useMemo } from "react";
 import { StyleSheet, ScrollView } from "react-native";
-import GenreList from "../components/Genres/GenreList";
 import Header from "../components/Header";
+import MovieList from "../components/Movie/MovieList";
+import { MOVIES_TYPE } from "../constants";
 import { RootStackParamList } from "../types";
 
 type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
@@ -16,7 +17,9 @@ function HomeScreen(props: HomeScreenProps) {
     <ScrollView contentContainerStyle={styles.container}>
       <StatusBar translucent backgroundColor={theme.colors.background} />
       <Header title="Now Playing" />
-      <GenreList />
+      <MovieList type={MOVIES_TYPE[0]} />
+      <Header title="Comming Soon" />
+      <MovieList type={MOVIES_TYPE[1]} />
     </ScrollView>
   );
 }
