ffb0831773ca4cbb3fd377e39d95a68dae110f05
Eunbi Kang <jenabill@naver.com>
2022-07-07 02:03:40 +0900

    Refactor: not using theme of react navigation in HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,35 +1,31 @@
-import { Theme, useTheme } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import { useMemo } from "react";
 import { StyleSheet, ScrollView } from "react-native";
 import Header from "../components/Header";
 import MovieList from "../components/Movie/MovieList";
 import { MOVIES_TYPE } from "../constants";
+import COLORS from "../styles/color";
 import { RootStackParamList } from "../types";
 
-type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
+export type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
 
-function HomeScreen(props: HomeScreenProps) {
-  const theme = useTheme();
-  const styles = useMemo(() => makeStyles(theme), []);
+function HomeScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <StatusBar translucent backgroundColor={theme.colors.background} />
+      <StatusBar translucent backgroundColor={COLORS.background} />
       <Header title="Now Playing" />
       <MovieList type={MOVIES_TYPE[0]} />
       <Header title="Comming Soon" />
-      <MovieList type={MOVIES_TYPE[1]} />
+      <MovieList type={MOVIES_TYPE[1]} size={0.7} />
     </ScrollView>
   );
 }
 
-const makeStyles = (theme: Theme) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.background,
+    paddingBottom: 100,
+  },
+});
 
 export default HomeScreen;
