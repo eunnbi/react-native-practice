56af2a0fa084cbd7b70d674a5b3673e6c311eab3
Eunbi Kang <jenabill@naver.com>
2022-07-07 02:01:48 +0900

    Refactor: change return value of getMovieId function calling api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,10 @@ import { MOVIES_TYPE } from "../constants";
 
 const API_KEY = process.env.API_KEY;
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-
 const MOVIES_ENDPOINTS = {
   [MOVIES_TYPE[0]]: "/movie/now_playing",
   [MOVIES_TYPE[1]]: "/movie/upcoming",
 };
-
 const TMDB_REQUEST = axios.create({
   baseURL: TMDB_BASE_URL,
   params: {
@@ -21,10 +19,15 @@ const getMovies = async (type: string) => {
   return data;
 };
 
-const getMovieById = (movieId: number, append_to_response: string = "") =>
-  TMDB_REQUEST.get(
+const getMovieById = async (
+  movieId: number,
+  append_to_response: string = ""
+) => {
+  const { data } = await TMDB_REQUEST.get(
     `/movie/${movieId}`,
     append_to_response ? { params: { append_to_response } } : undefined
   );
+  return data;
+};
 
 export { getMovies, getMovieById };
