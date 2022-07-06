9909e66de7edaa1916279dfb7ca709eb498e08f8
Eunbi Kang <jenabill@naver.com>
2022-07-07 02:01:12 +0900

    Refactor: remove unnecessary code

--- src/components/Movie/useMovieInfo.ts
+++ src/components/Movie/useMovieInfo.ts
@@ -2,14 +2,6 @@ import { useQuery } from "react-query";
 import { getMovieById } from "../../api";
 import { IMovieWithVideo } from "../../types";
 
-export const APPEND_TO_RESPONSE = {
-  DETAILS: "",
-  VIDEOS: "videos",
-  CREDITS: "credits",
-  RECOMMENDATIONS: "recommendations",
-  SIMILAR: "similar",
-};
-
 export function useMovieInfo(id: number, type: string = "") {
   const query = useQuery<IMovieWithVideo>(["movie", id, type], () =>
     getMovieById(id, type)
