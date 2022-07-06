383041bbbe0cc847a5b1be689c917d25777bbbbf
Eunbi Kang <jenabill@naver.com>
2022-07-07 02:00:18 +0900

    Refactor: add type of useQuery data fetching movie list

--- src/components/Movie/useMovieList.ts
+++ src/components/Movie/useMovieList.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "react-query";
 import { getMovies } from "../../api";
+import { IMovieList } from "../../types";
 
 export const useMovieList = (type: string) => {
-  const query = useQuery(["movies", type], () => getMovies(type));
+  const query = useQuery<IMovieList>(["movies", type], () => getMovies(type));
   return query;
 };
