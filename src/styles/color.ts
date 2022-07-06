2fa488367a16670a65ca3c899dc5b2a2d3d75b66
Eunbi Kang <jenabill@naver.com>
2022-07-07 01:58:21 +0900

    Design: change secondLightGray color

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,10 +1,11 @@
 const COLORS = {
+  background: "#f5f7fa",
   active: "#057dfe",
   heart: "#f13939",
   yellow: "#ffc107",
   gray: "#6B6B70",
   lightGray: "#969696",
-  secondLightGray: "#e5e5e5",
+  secondLightGray: "#EBEDF3",
 };
 
 export default COLORS;
