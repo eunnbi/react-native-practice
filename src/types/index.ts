4c9c03f8a84f9f7fee03ed533f6756773a1a8783
Eunbi Kang <jenabill@naver.com>
2022-07-07 01:57:56 +0900

    Refactor: add types related to Movie data

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,49 @@
 export type RootStackParamList = {
   Home: undefined;
-  Movie: undefined;
+  Movie: {
+    id: number;
+    title: string;
+    language: string | undefined;
+    image: string;
+  };
 };
+
+export interface IMovieList {
+  results: IMovie[];
+}
+
+export interface IMovie {
+  id: number;
+  genre_ids: number[];
+  genres: Genre[];
+  original_language: string;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  vote_count: number;
+  backdrop_path: string;
+  runtime?: number;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface IMovieWithVideo extends IMovie {
+  videos: IMovieVideoList;
+}
+
+interface IMovieVideoList {
+  results: IMovieVideo[];
+}
+
+export interface IMovieVideo {
+  key: string;
+  site: string;
+  type: string;
+}
