f1107935ee31043945a04a003ade8a9b2d0a0678
Eunbi Kang <jenabill@naver.com>
2022-07-07 02:02:33 +0900

    Refactor: binding movie data to MovieCard component

--- src/components/Movie/MovieList.tsx
+++ src/components/Movie/MovieList.tsx
@@ -1,28 +1,42 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import ItemSeperator from "../common/ItemSeperator";
 import MovieCard from "./MovieCard";
 import { useMovieList } from "./useMovieList";
 
-const dummy = ["asdf", "Asdf", "asdf", "asdfasdf"];
+interface MovieListProps {
+  type: string;
+  size: number;
+}
 
-export default function MovieList({ type }: { type: string }) {
+export default function MovieList({ type, size }: MovieListProps) {
   const { status, data } = useMovieList(type);
+  if (status === "loading") {
+    <View>
+      <ActivityIndicator />
+    </View>;
+  }
   return (
     <View style={styles.container}>
       <FlatList
-        data={dummy}
+        data={data?.results}
         horizontal
         showsHorizontalScrollIndicator={false}
-        keyExtractor={(item, index) => index.toString()}
-        ItemSeparatorComponent={() => <ItemSeperator width={20} />}
-        ListHeaderComponent={() => <ItemSeperator width={20} />}
-        ListFooterComponent={() => <ItemSeperator width={20} />}
-        renderItem={({ item }) => <MovieCard item={item} />}
+        keyExtractor={({ id }) => id.toString()}
+        ItemSeparatorComponent={() => <ItemSeperator width={30 * size} />}
+        ListHeaderComponent={() => <ItemSeperator width={30 * size} />}
+        ListFooterComponent={() => <ItemSeperator width={30 * size} />}
+        renderItem={({ item }) => (
+          <MovieCard movie={item} size={size ? size : 1} />
+        )}
       />
     </View>
   );
 }
+
+MovieList.defaultProps = {
+  size: 1,
+};
 
 const styles = StyleSheet.create({
   container: {
