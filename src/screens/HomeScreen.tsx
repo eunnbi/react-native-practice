2d2d7cfbc3547a003fe2aec5c5514e189f16d312
Eunbi Kang <jenabill@naver.com>
2022-07-07 23:09:24 +0900

    Refactor: remove Header component in HomeScreen component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,17 @@
+import { Ionicons } from "@expo/vector-icons";
 import { StackScreenProps } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ScrollView } from "react-native";
-import Header from "../components/Header";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import MovieList from "../components/MovieList";
 import { MOVIES_TYPE } from "../constants";
 import COLORS from "../styles/color";
+import FONTS from "../styles/fonts";
 import { RootStackParamList } from "../types";
 
 export type HomeScreenProps = StackScreenProps<RootStackParamList, "Home">;
@@ -13,9 +20,14 @@ function HomeScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <StatusBar translucent backgroundColor={COLORS.background} />
-      <Header title="Now Playing" />
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>Now Playing</Text>
+      </View>
       <MovieList type={MOVIES_TYPE[0]} />
-      <Header title="Comming Soon" />
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>Comming Soon</Text>
+      </View>
+
       <MovieList type={MOVIES_TYPE[1]} size={0.7} />
     </ScrollView>
   );
@@ -25,6 +37,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.background,
     paddingBottom: 100,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 60,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: FONTS.REGULAR,
   },
 });
 
