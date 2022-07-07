a98da7a20f09ac939b3f24645131b60151a9e230
Eunbi Kang <jenabill@naver.com>
2022-07-07 23:09:54 +0900

    Refactor: use ActivityIndicator instead of AppLoading

--- App.tsx
+++ App.tsx
@@ -5,12 +5,11 @@ import HomeScreen from "./src/screens/HomeScreen";
 import MovieScreen from "./src/screens/MovieScreen";
 import { RootStackParamList } from "./src/types";
 import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ActivityIndicator } from "react-native";
 
 const RootStack = createStackNavigator<RootStackParamList>();
 const queryClient = new QueryClient();
-
 export default function App() {
   const [fontLoaded] = useFonts({
     Regular: require("./assets/fonts/NunitoSans-Regular.ttf"),
@@ -23,7 +22,7 @@ export default function App() {
   });
 
   if (!fontLoaded) {
-    return <AppLoading />;
+    return <ActivityIndicator />;
   }
 
   return (
